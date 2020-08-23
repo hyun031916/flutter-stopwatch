38cea77fa6b6b6dc1e408ead4b0d018a7b98107c
ksh <hyun031916@gmail.com>
2020-08-23 14:38:07 +0900

    #3 타이머 영역 UI 작성

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,54 @@ class _StopWatchPageState extends State<StopWatchPage> {
   }
 
   Widget _buildBody(){
-    return Container();
+    return Container(
+      child: Padding(
+        padding:const EdgeInsets.only(top:30),
+        child: Stack(
+          children: <Widget>[
+            Column(
+              children:<Widget>[
+                Row(  //시간을 표시하는 영역
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Text( //초
+                      '0',
+                      style: TextStyle(fontSize: 50.0),
+                    ),
+                    Text('00'), //1/100초
+                  ],
+                ),
+                Container(
+                  width: 100,
+                  height: 200,
+                  child: ListView(
+                    children: <Widget>[], // TODO : 랩타임 추후 수정
+                  )
+                )
+              ]
+            ),
+            Positioned(
+              left: 10,
+              bottom: 10,
+              child: FloatingActionButton(
+                backgroundColor: Colors.deepOrangeAccent,
+                onPressed: (){}, //TODO : 왼쪽 아래 위치 초기화 버튼 동작 구현
+                child: Icon(Icons.rotate_left),
+              ),
+            ),
+            Positioned( //오른쪽 아래에 위치한 랩타임 버튼
+              right: 10,
+              bottom: 10,
+              child : RaisedButton(
+                onPressed:(){}, //TODO: 오른 쪽 아래에 위치한 랩타임 버튼 동작 구현
+                child: Text('랩타임')
+              )
+            )
+          ]
+        )
+      )
+    );
   }
 
   void _clickButton(){
